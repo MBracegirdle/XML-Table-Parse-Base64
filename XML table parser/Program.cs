9914dc6e368ef59cadbc25cf0d169e265d723781
MatBracegirdle <mattjbracegirdle@gmail.com>
2025-12-15 20:43:07 +0000

    Added table selection

--- XML table parser/Program.cs
+++ XML table parser/Program.cs
@@ -21,27 +21,46 @@ s = File.Open(fullXMLPath, FileMode.Open);
 
 DataSet ds = new DataSet();
 ds.ReadXml(s);
+
+Console.WriteLine("-- Tables --");
+for (int i = 0; i < ds.Tables.Count; i++)
+{
+    Console.WriteLine(i.ToString()+". "+ ds.Tables[i].TableName);
+}
+int tabL = int.Parse(Console.ReadLine());
+
+
+
 dt = ds.Tables[0];
+Console.Clear();
+Console.WriteLine("-- Table Headers:enter number --");
+
 for (int i = 0; i < dt.Columns.Count; i++)
 {
-    Console.WriteLine(dt.Columns[i].ColumnName);
+    Console.WriteLine(i.ToString() + ". " + dt.Columns[i].ColumnName);
 }
 
 
 Console.WriteLine("Filename column:");
-string fnc = Console.ReadLine();
+int fnc = int.Parse(Console.ReadLine());
 Console.WriteLine("Base64 column:");
-string b64c = Console.ReadLine();
+int b64c = int.Parse(Console.ReadLine());
 oPath:
 Console.WriteLine("Enter output path:");
 string outPath = Console.ReadLine();
 if (Path.Exists(outPath) == false) { goto oPath; }
 outPath =  Path.Combine(outPath, "Documents" );
+Console.WriteLine(outPath);
+Directory.CreateDirectory(outPath);
+Console.WriteLine(Directory.Exists(outPath));
+Console.WriteLine(dt.Rows.Count);
 for (int i = 0; i < dt.Rows.Count; i++)
 {
+    Console.WriteLine("Row "+i.ToString());
     string fn = (string)dt.Rows[i][fnc];
-
-    File.WriteAllBytes(Path.Combine(outPath,fn), Convert.FromBase64String( (string)dt.Rows[i][b64c]));
-
-
+    string b64s = (string)dt.Rows[i][b64c];
+    Console.WriteLine("Base 64 string read");
+    File.WriteAllBytes(Path.Combine(outPath,fn), Convert.FromBase64String(b64s));
+    Console.WriteLine(fn);
+    b64s = null;
         }
